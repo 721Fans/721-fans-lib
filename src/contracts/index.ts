c7cb5d35e65f828cd0bfef0e277d42fa382498dd
PhilippLgh <philipp@ethereum.org>
2021-01-21 16:43:22 +0100

    add forwarder helper

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,4 +1,15 @@
+import { ethers } from 'ethers'
 import * as FAN from './FanToken'
+import { configurations } from '../config'
 export { FAN }
 
 export * from './IdentityProvider'
+
+const { contracts, url } = configurations.xDai
+
+export const getForwarderContract = async () => {
+  const provider = new ethers.providers.JsonRpcProvider(url)
+  const { address, abi } = contracts.Forwarder
+  const contract = new ethers.Contract(address, abi, provider);
+  return contract
+}
